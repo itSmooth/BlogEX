3775dbf9ae157e5ada49bfa71e15b73470b94018
IT Smooth <itsmooth.tw@gmail.com>
2020-05-30 21:41:12 +0800

    複製 _Layout.cshtml、_LayoutAdmin.cshtml。安裝 EF Core、新增兩張資料表對應[Articles] [TagCloud]、新增RazorPageBlogContext 類別，注入DbContext

---
+++ BlogEX/Data/RazorPageBlogContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BlogEX.Data
+{
+    public partial class RazorPageBlogContext : DbContext
+    {
+        public RazorPageBlogContext()
+        {
+        }
+        public RazorPageBlogContext(DbContextOptions<RazorPageBlogContext> options)
+            : base(options)
+        {
+        }
+   
+        public virtual DbSet<Articles> Articles { get; set; }
+        public virtual DbSet<TagCloud> TagCloud { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Articles>(entity => { entity.Property(e => e.Id).ValueGeneratedNever(); });
+
+            modelBuilder.Entity<TagCloud>(entity => { entity.Property(e => e.Id).ValueGeneratedNever(); });
+
+            //OnModelCreatingPartial(modelBuilder);
+        }
+        //partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
