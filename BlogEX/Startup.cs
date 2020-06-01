652f1d3dc71b7db3df46e55aff0657f21d6b797f
IT Smooth <itsmooth.tw@gmail.com>
2020-06-01 16:31:00 +0800

    修好 <TagsHelper> 不會變綠色的問題

--- BlogEX/Startup.cs
+++ BlogEX/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using BlogEX.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,6 +30,8 @@ namespace BlogEX
                 options =>
                     options.UseSqlServer(
                         Configuration.GetConnectionString("DefaultConnection")));
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
