b64f0516f28f31773299d5e2b5cc8fee94b14ae8
IT Smooth <itsmooth.tw@gmail.com>
2020-06-01 15:44:13 +0800

    加入 TagsHelper ，但是<tags-helper>不會變綠色

---
+++ BlogEX/Helper/Tag/TagsHelper.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogEX.Helper.Tag
+{
+    public class TagsHelper:TagHelper
+    {
+        private IUrlHelperFactory UrlHelperFactory { get; }
+        private IActionContextAccessor Accessor { get; }
+        public TagsHelper(IUrlHelperFactory urlHelperFactory, IActionContextAccessor accessor)
+        {
+            UrlHelperFactory = urlHelperFactory;
+            Accessor = accessor;
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var actionContext = Accessor.ActionContext;
+            var urlHelper = UrlHelperFactory.GetUrlHelper(actionContext);
+            output.TagName = "ul";
+            var content = await output.GetChildContentAsync();
+            if (string.IsNullOrWhiteSpace(content.GetContent()) == false)
+            {
+                var tags = content.GetContent().Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                var sb = new StringBuilder();
+                foreach (var tag in tags)
+                {
+                    sb.AppendFormat("<li class='d-inline p-1'><a href='{0}' class='btn btn-success'>{1}</a></li>",
+                                    urlHelper.Page("index", "tag", new { tag = tag }), tag);
+                }
+
+                output.Content.SetHtmlContent(sb.ToString());
+            }
+        }
+    }
+}
--- BlogEX/Pages/Index.cshtml.cs
+++ BlogEX/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlogEX.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,14 +12,38 @@ namespace BlogEX.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly RazorPageBlogContext _context;
+        public IList<MyClass> Article { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public class MyClass
+        {
+            public Guid Id { get; set; }
+            public string CoverPhoto { get; set; }
+            public string Title { get; set; }
+            public string Body { get; set; }
+            public string Tags { get; set; }
+            public DateTime CreateDate { get; set; }
+        }
+
+        public IndexModel(ILogger<IndexModel> logger, RazorPageBlogContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public void OnGet()
         {
+            Article = _context.Articles.Select(d => 
+                new MyClass
+                    {
+                        Id = d.Id,
+                        CoverPhoto = d.CoverPhoto,
+                        Title = d.Title,
+                        Body = d.Body.Substring(0, 200),
+                        CreateDate = d.CreateDate,
+                        Tags = d.Tags
+                    })
+                .ToList();
 
         }
     }
